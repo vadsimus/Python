622b9c58763d7ec8757e11a516f3069afcae9fe4
vadsimus <46888307+vadsimus@users.noreply.github.com>
2019-09-29 09:19:45 +0300

    Update Count_triplets.py

--- Count Triplets/Count_triplets.py
+++ Count Triplets/Count_triplets.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-#  https://www.hackerrank.com/challenges/count-triplets-1/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=dictionaries-hashmaps
+#  https://www.hackerrank.com/challenges/count-triplets-1/problem
 
 import math
 import os
