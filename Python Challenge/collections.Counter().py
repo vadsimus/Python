f0c013b7e6515179c31f06e006a83b5b2f75b5b1
vadsimus <46888307+vadsimus@users.noreply.github.com>
2019-10-11 09:58:40 +0300

    Update collections.Counter().py

--- Python Challenge/collections.Counter().py
+++ Python Challenge/collections.Counter().py
@@ -1,4 +1,5 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
+# https://www.hackerrank.com/challenges/collections-counter/problem
+
 from collections import Counter
 
 ss = []
