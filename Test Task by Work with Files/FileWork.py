093019306821a1be5a4869767a8597b1eb70896f
vadsimus <vadsimus@gmail.com>
2019-10-18 20:58:53 +0300

    Browsers --> OS  counting

--- Test Task by Work with Files/FileWork.py
+++ Test Task by Work with Files/FileWork.py
@@ -1,12 +1,12 @@
 import re
 from collections import defaultdict as dd
+
 while True:
     file_name = input('File name:')
 
     try:
         with open(file_name, 'r') as file:
             ips = dd(int)
-            # browsers = dd(int)
 
             oss = dd(int)
             for line in file:
@@ -14,10 +14,10 @@ while True:
                     ip = re.findall(r'^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', line)
                     ip = ip[0]
                 except IndexError:
-                    continue #IP is not found, this is wrong line, pass it.
+                    continue  # IP is not found, this is wrong line, pass it.
                 else:
                     ips[ip] += 1
-                   
+
                 browser = re.findall(r'[^""]+', line)
 
                 try:
@@ -25,43 +25,38 @@ while True:
                 except IndexError:
                     pass
                 else:
-                    pass
-                try:
-                    os=re.split(r';', re.split(r'[()]', browser)[1])
-
-                except IndexError:
-                    pass
-                else:
-                    if os[0]=='compatible':
-                        os=None
-                    elif os[0]=='X11':
-                        os=os[1]
+                    try:
+                        os = re.split(r';', re.split(r'[()]', browser)[1])
+                    except IndexError:
+                        pass
                     else:
-                        os=os[0]
-                    if os:
-                        os=[os.split()][0][0]
-                        oss[os]+=1
+                        if os[0] == 'compatible':
+                            os = None
+                        elif os[0] == 'X11':
+                            os = os[1]
+                        else:
+                            os = os[0]
+                        if os:
+                            os = [os.split()][0][0]
+                            oss[os] += 1
 
-
-               
-            break #Stop cycle of file name input
+            break  # Stop cycle of file name input
     except FileNotFoundError:
         print('File not found')
 
 print('Access:')
 ip_count = [x for x in ips.values()]
 ip_count.sort(reverse=True)
-for i in range(10) if len(ip_count)>10 else range(len(ip_count)):
-    ip_found=[x for x in ips.keys() if ips[x] == ip_count[i]][0]
+for i in range(10) if len(ip_count) > 10 else range(len(ip_count)):
+    ip_found = [x for x in ips.keys() if ips[x] == ip_count[i]][0]
     print(ip_found.ljust(15), end='')
-    print(':',ips[ip_found],'times')
-    ips[ip_found]=0
+    print(':', ips[ip_found], 'times')
+    ips[ip_found] = 0
 print('----------')
 print('Most popular OS:')
-os_count=[x for x in oss.values()]
+os_count = [x for x in oss.values()]
 os_count.sort(reverse=True)
-for i in range(5) if len(os_count)>5 else range(len(os_count)):
-    os_found=[x for x in oss.keys() if oss[x] == os_count[i]][0]
-    print(os_found.ljust(15),':',oss[os_found],'times')
-    oss[os_found]=0
-
+for i in range(5) if len(os_count) > 5 else range(len(os_count)):
+    os_found = [x for x in oss.keys() if oss[x] == os_count[i]][0]
+    print(os_found.ljust(15), ':', oss[os_found], 'times')
+    oss[os_found] = 0
