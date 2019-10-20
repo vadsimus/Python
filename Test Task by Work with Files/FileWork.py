9b321335d5d82e32fcb698c9d89db91d61aca9cf
vadsimus <vadsimus@gmail.com>
2019-10-20 10:29:22 +0300

    Browsers --> OS  counting

--- Test Task by Work with Files/FileWork.py
+++ Test Task by Work with Files/FileWork.py
@@ -22,43 +22,39 @@ while True:
 
                 try:
                     browser = [browser[-2]][0]
-                except IndexError:
-                    browser = None
-                else:
-                    try:
-                        os = re.split(r';', re.split(r'[()]', browser)[1])
-                    except IndexError:
+                    os = re.split(r';', re.split(r'[()]', browser)[1])
+                    if 'compatible' in ' '.join(os):
                         os = None
-                    else:
-                        if 'compatible' in ' '.join(os):
-                            os = None
-                        elif os[0] == 'X11':
-                            if os[1] == ' U':
-                                os = os[2]
-                            else:
-                                os = os[1]
+                    elif os[0] == 'X11':
+                        if os[1] == ' U':
+                            os = os[2]
                         else:
-                            os = os[0]
-                        if os and browser:
-                            os = [os.split()][0][0]
-                            oss[os] += 1
+                            os = os[1]
+                    else:
+                        os = os[0]
+                except IndexError:
+                    os = None
+                else:
+                    if os:
+                        os = [os.split()][0][0]
+                        oss[os] += 1
 
             break  # Stop cycle of file name input
     except FileNotFoundError:
-        print('File not found')
+        print('File not found, try again')
 
 print('Access:')
 ip_count = [x for x in ips.values()]
 ip_count.sort(reverse=True)
 for i in range(10) if len(ip_count) > 10 else range(len(ip_count)):
     ip_found = [x for x in ips.keys() if ips[x] == ip_count[i]][0]
-    print(ip_found.ljust(15), ':', ips[ip_found], 'times')
+    print(ip_found.ljust(20), ':', ips[ip_found], 'times')
     ips[ip_found] = 0
-print('----------')
+print('-'*20)
 print('Most popular OS:')
 os_count = [x for x in oss.values()]
 os_count.sort(reverse=True)
 for i in range(5) if len(os_count) > 5 else range(len(os_count)):
     os_found = [x for x in oss.keys() if oss[x] == os_count[i]][0]
-    print(os_found.ljust(15), ':', oss[os_found], 'times')
+    print(os_found.ljust(20), ':', oss[os_found], 'times')
     oss[os_found] = 0
